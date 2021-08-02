1ecda6b42fd9a3fd657646efe9b4299a274f8023
hayktermarkosyan <hayktermarkosyan@gmail.com>
2021-08-02 12:17:01 +0400

    with resizable imgCropper

--- script.js
+++ script.js
@@ -63,8 +63,6 @@ cropBtn.addEventListener('click', () => {
         imgCropper.offsetLeft * correctionX, imgCropper.offsetTop * correctionY, imgCropper.offsetWidth * correctionX, imgCropper.offsetHeight * correctionY,
         left, top, width, height);
 
-    console.log(correctionX)
-    console.log(correctionY)
     img.style.display = 'none';
     canvas.style.display = 'inline';
     imgCropper.style.display = 'none';
@@ -109,3 +107,83 @@ function moveImgCropper(e) {
     imgCropper.style.left = `${x}px`;
     imgCropper.style.top = `${y}px`;
 }
+
+
+// resizable imgCropper
+
+function makeResizableDiv(div) {
+    const element = document.querySelector(div);
+    const resizers = document.querySelectorAll(div + ' .resizer');
+    const minimum_size = 20;
+    let original_width = 0;
+    let original_height = 0;
+    let original_x = 0;
+    let original_y = 0;
+    let original_mouse_x = 0;
+    let original_mouse_y = 0;
+    for (let i = 0; i < resizers.length; i++) {
+        const currentResizer = resizers[i];
+        currentResizer.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            original_width = parseFloat(getComputedStyle(element).getPropertyValue('width').replace('px', ''));
+            original_height = parseFloat(getComputedStyle(element).getPropertyValue('height').replace('px', ''));
+            original_x = element.getBoundingClientRect().left;
+            original_y = element.getBoundingClientRect().top;
+            original_mouse_x = e.pageX;
+            original_mouse_y = e.pageY;
+            window.addEventListener('mousemove', resize);
+            window.addEventListener('mouseup', stopResize);
+        })
+
+        function resize(e) {
+            isCropperMoving = false;
+            if (currentResizer.classList.contains('bottom-right')) {
+                const width = original_width + (e.pageX - original_mouse_x);
+                const height = original_height + (e.pageY - original_mouse_y);
+                if (width > minimum_size) {
+                    element.style.width = width + 'px';
+                }
+                if (height > minimum_size) {
+                    element.style.height = height + 'px';
+                }
+            } else if (currentResizer.classList.contains('bottom-left')) {
+                const height = original_height + (e.pageY - original_mouse_y);
+                const width = original_width - (e.pageX - original_mouse_x);
+                if (height > minimum_size) {
+                    element.style.height = height + 'px';
+                }
+                if (width > minimum_size) {
+                    element.style.width = width + 'px';
+                    element.style.left = original_x + (e.pageX - original_mouse_x) + 'px';
+                }
+            } else if (currentResizer.classList.contains('top-right')) {
+                const width = original_width + (e.pageX - original_mouse_x);
+                const height = original_height - (e.pageY - original_mouse_y);
+                if (width > minimum_size) {
+                    element.style.width = width + 'px';
+                }
+                if (height > minimum_size) {
+                    element.style.height = height + 'px';
+                    element.style.top = original_y + (e.pageY - original_mouse_y) + 'px';
+                }
+            } else {
+                const width = original_width - (e.pageX - original_mouse_x);
+                const height = original_height - (e.pageY - original_mouse_y);
+                if (width > minimum_size) {
+                    element.style.width = width + 'px';
+                    element.style.left = original_x + (e.pageX - original_mouse_x) + 'px';
+                }
+                if (height > minimum_size) {
+                    element.style.height = height + 'px';
+                    element.style.top = original_y + (e.pageY - original_mouse_y) + 'px';
+                }
+            }
+        }
+
+        function stopResize() {
+            window.removeEventListener('mousemove', resize);
+        }
+    }
+}
+
+makeResizableDiv('.resizable');
